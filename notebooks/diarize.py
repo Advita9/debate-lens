8c00c103deefb3943bb1c83cc25541c560da9371
Advita <advitashrivastava09@gmail.com>
2025-06-07 01:41:24 +0530

    speaker segmentation using hugging face pyannote

--- notebooks/diarize.py
+++ notebooks/diarize.py
@@ -1,8 +1,16 @@
 from pyannote.audio import Pipeline
+from dotenv import load_dotenv
+import os
 
-pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token="REMOVED")
+load_dotenv()
+
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
+
+pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization", use_auth_token=HUGGINGFACE_TOKEN)
 
 diarization = pipeline("../harvard.wav")
+
+print(f"Hugging Face token loaded: {HUGGINGFACE_TOKEN[:5]}...") 
 
 for turn, _, speaker in diarization.itertracks(yield_label = True):
     print(f"{turn.start:.1f}s - {turn.end:.1f}s: {speaker}")
